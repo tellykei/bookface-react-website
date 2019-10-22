922ca0b3498325e2d7bf99a842de6e832fbf6102
Taliyah Rivera <trive2@bitbucket.org>
2019-10-22 17:14:45 -0400

    like/dislike feature

--- bookface/src/components/HomePage.js
+++ bookface/src/components/HomePage.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import { IconButton } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import { Card, CardContent, CardActions, Button } from '@material-ui/core';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbDownAltIcon from "@material-ui/icons/ThumbDownAlt";
 
 class HomePage extends React.Component {
-    
+    constructor(props){
+        super(props);
+        this.state = {
+            like: 0,
+            dislike:0,
+            updatedlike:false,
+            updateddislike:false
+
+        };
+        this.handelAddlike=this.handelAddlike.bind(this);
+        this.handleAdddislike=this.handleAdddislike.bind(this);
+    }
+    handelAddlike(event){
+        const{like}=this.state;
+        this.setState({like:like +1})
+        this.setState({updatedlike:!this.state.updatedlike})
+    }
+    handleAdddislike(event){
+        const{dislike}=this.state;
+        this.setState({dislike:dislike+1})
+        this.setState({updatedlike:!this.state.updateddislike})
+    }
     render() {
 
         const cards = ['Fry', 'Leela', 'Zoidberg'].map((name) => {
@@ -31,17 +54,21 @@ class HomePage extends React.Component {
                     </CardContent>
 
                     <CardActions>
-                        <Button variant={'contained'}
-                                size={'small'}
-                                color={'primary'}>
-
-                            Like
-                        </Button>
-                        <Button variant={'contained'}
-                                size={'small'}
-                                color={'secondary'}>
-                            Dislike
-                        </Button>
+                    <IconButton 
+                        variant={"contained"}
+                        aria-label="like" 
+                        onClick={this.handelAddlike}>
+                    <ThumbUpIcon />
+                     {this.state.like}
+                    </IconButton>
+      
+                 <IconButton
+                    variant = {"contained"}
+                    aria-label="dislike"
+                    onClick={this.handleAdddislike}>
+                <ThumbDownAltIcon/>
+                    {this.state.dislike}
+                 </IconButton>
                     </CardActions>
                 </Card>
             )
