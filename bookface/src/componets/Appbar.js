2c620e063728090cd4a28a64e069ee69b59ca395
rpmorton29 <rmort1@unh.newhaven.edu>
2019-10-21 20:25:06 -0400

    loginpage and attempt at hierarchy

---
+++ bookface/src/componets/Appbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+
+class Appbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">News</Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default Appbar;
--- bookface/src/index.js
+++ bookface/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import Appbar from "./componets/Appbar";
+import Loginpage from "./componets/loginpage";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//ReactDOM.render(<Appbar />, document.getElementById("root"));
+ReactDOM.render(<Loginpage />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
